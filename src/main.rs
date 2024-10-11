84e40a22631985c1f23e139aaeecc7fe94bf9440
kigi <kigi.chang@gmail.com>
2024-10-11 16:43:03 +0800

    download test models from huggingface hub

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,50 @@
 use candle_core::{DType, Device, Tensor, D};
 use candle_nn::{rnn, GRUConfig, LSTMConfig, VarBuilder, RNN};
+use hf_hub::{
+    api::sync::{Api, ApiRepo},
+    Repo, RepoType,
+};
 
 use anyhow::Result;
 
-static IN_DIM: usize = 10;
-static HIDDEN_DIM: usize = 20;
-static BATCH_SIZE: usize = 5;
-static SEQ_LEN: usize = 3;
+const ACCURACY: f32 = 1e-6;
 
-struct Args {
-    input_dim: usize,
-
-    hidden_dim: usize,
-
-    layers: usize,
-
-    batch_size: usize,
-
-    seq_len: usize,
-
-    pt: &'static str,
+#[derive(Debug, Clone, PartialEq, serde::Deserialize)]
+struct Config {
+    pub input: usize,
+    pub batch_size: usize,
+    pub sequence_length: usize,
+    pub hidden: usize,
+    pub layers: usize,
+    pub bidirection: bool,
 }
 
-impl Args {
-    pub fn load_pt(&self) -> Result<VarBuilder<'static>> {
-        Ok(VarBuilder::from_pth(self.pt, DType::F32, &Device::Cpu)?)
-    }
+fn repo() -> Result<ApiRepo> {
+    let api = Api::new()?;
+    let repo_id = "kigichang/test_rnn".to_string();
+    let repo = api.repo(Repo::with_revision(
+        repo_id,
+        RepoType::Model,
+        "main".to_string(),
+    ));
+    Ok(repo)
 }
 
-fn assert_tensor(a: &Tensor, b: &Tensor, dim: usize, v: f32) -> Result<()> {
+fn load_model(model: &str, device: &Device) -> Result<(Config, VarBuilder<'static>)> {
+    let repo = repo()?;
+
+    let filename = repo.get(&format!("{}.pt", model))?;
+    let config_file = repo.get(&format!("{}.json", model))?;
+
+    let config: Config = serde_json::from_slice(&std::fs::read(config_file)?)?;
+    let vb = VarBuilder::from_pth(filename, DType::F32, device)?;
+
+    Ok((config, vb))
+}
+
+fn assert_tensor(a: &Tensor, b: &Tensor, v: f32) -> Result<()> {
     assert_eq!(a.dims(), b.dims());
+    let dim = a.dims().len();
     let mut t = (a - b)?.abs()?;
 
     for _i in 0..dim {
@@ -37,7 +52,7 @@ fn assert_tensor(a: &Tensor, b: &Tensor, dim: usize, v: f32) -> Result<()> {
     }
 
     let t = t.to_scalar::<f32>()?;
-    println!("max diff: {}", t);
+    println!("max diff = {}", t);
     assert!(t < v);
     Ok(())
 }
@@ -56,24 +71,31 @@ fn gru_config(index: usize, direction: rnn::Direction) -> GRUConfig {
     config
 }
 
-fn run_lstm(args: Args) -> Result<(Tensor, Tensor)> {
-    let vb = args.load_pt()?;
-    let answer = vb.get((args.batch_size, args.seq_len, args.hidden_dim), "output")?;
+fn run_lstm(model: &str, device: &Device) -> Result<(Tensor, Tensor)> {
+    let (config, vb) = load_model(model, device)?;
 
-    let mut layers = Vec::with_capacity(args.layers);
+    let answer = vb.get(
+        (config.batch_size, config.sequence_length, config.hidden),
+        "output",
+    )?;
 
-    for layer_idx in 0..args.layers {
+    let mut layers = Vec::with_capacity(config.layers);
+
+    for layer_idx in 0..config.layers {
         let input_dim = if layer_idx == 0 {
-            args.input_dim
+            config.input
         } else {
-            args.hidden_dim
+            config.hidden
         };
-        let config = lstm_config(layer_idx, rnn::Direction::Forward);
-        let lstm = candle_nn::lstm(input_dim, args.hidden_dim, config, vb.clone())?;
+        let lstm_config = lstm_config(layer_idx, rnn::Direction::Forward);
+        let lstm = candle_nn::lstm(input_dim, config.hidden, lstm_config, vb.clone())?;
         layers.push(lstm);
     }
 
-    let mut input = vb.get((args.batch_size, args.seq_len, args.input_dim), "input")?;
+    let mut input = vb.get(
+        (config.batch_size, config.sequence_length, config.input),
+        "input",
+    )?;
 
     for layer in &layers {
         let states = layer.seq(&input)?;
@@ -83,115 +105,89 @@ fn run_lstm(args: Args) -> Result<(Tensor, Tensor)> {
     Ok((input, answer))
 }
 
-fn run_bidirectional_lstm(args: Args) -> Result<(Tensor, Tensor)> {
-    let vb = args.load_pt()?;
+fn run_bidirectional_lstm(model: &str, device: &Device) -> Result<(Tensor, Tensor)> {
+    let (config, vb) = load_model(model, device)?;
+
     let answer = vb.get(
-        (args.batch_size, args.seq_len, args.hidden_dim * 2),
+        (config.batch_size, config.sequence_length, config.hidden * 2),
         "output",
     )?;
 
-    let mut layers = Vec::with_capacity(args.layers);
+    let mut layers = Vec::with_capacity(config.layers);
 
-    for layer_idx in 0..args.layers {
+    for layer_idx in 0..config.layers {
         let input_dim = if layer_idx == 0 {
-            args.input_dim
+            config.input
         } else {
-            args.hidden_dim * 2
+            config.hidden * 2
         };
 
         let forward_config = lstm_config(layer_idx, rnn::Direction::Forward);
-        let forward = candle_nn::lstm(input_dim, args.hidden_dim, forward_config, vb.clone())?;
+        let forward = candle_nn::lstm(input_dim, config.hidden, forward_config, vb.clone())?;
 
         let backward_config = lstm_config(layer_idx, rnn::Direction::Backward);
-        let backward = candle_nn::lstm(input_dim, args.hidden_dim, backward_config, vb.clone())?;
+        let backward = candle_nn::lstm(input_dim, config.hidden, backward_config, vb.clone())?;
 
         layers.push((forward, backward));
     }
 
-    let mut input = vb.get((args.batch_size, args.seq_len, args.input_dim), "input")?;
+    let mut input = vb.get(
+        (config.batch_size, config.sequence_length, config.input),
+        "input",
+    )?;
 
     for (forward, backward) in &layers {
         let forward_states = forward.seq(&input)?;
         let backward_states = backward.seq(&input)?;
-        input = forward.combine_states_to_tensor(&forward_states, &backward_states)?;
+        input = forward.bidirectional_states_to_tensor(&forward_states, &backward_states)?;
     }
     Ok((input, answer))
 }
 
 fn test_lstm() -> Result<()> {
-    let args = Args {
-        input_dim: IN_DIM,
-        hidden_dim: HIDDEN_DIM,
-        layers: 1,
-        batch_size: BATCH_SIZE,
-        seq_len: SEQ_LEN,
-        pt: "lstm_test.pt",
-    };
-
-    let (output, answer) = run_lstm(args)?;
-    assert_tensor(&output, &answer, 3, 0.0000001)
+    let (output, answer) = run_lstm("lstm_test", &Device::Cpu)?;
+    assert_tensor(&output, &answer, ACCURACY)
 }
 
 fn test_nlayer_lstm() -> Result<()> {
-    let args = Args {
-        input_dim: IN_DIM,
-        hidden_dim: HIDDEN_DIM,
-        layers: 3,
-        batch_size: BATCH_SIZE,
-        seq_len: SEQ_LEN,
-        pt: "lstm_nlayer_test.pt",
-    };
-
-    let (output, answer) = run_lstm(args)?;
-    assert_tensor(&output, &answer, 3, 0.0000001)
+    let (output, answer) = run_lstm("lstm_nlayer_test", &Device::Cpu)?;
+    assert_tensor(&output, &answer, ACCURACY)
 }
 
 fn test_bi_lstm() -> Result<()> {
-    let args = Args {
-        input_dim: IN_DIM,
-        hidden_dim: HIDDEN_DIM,
-        layers: 1,
-        batch_size: BATCH_SIZE,
-        seq_len: SEQ_LEN,
-        pt: "bi_lstm_test.pt",
-    };
-
-    let (output, answer) = run_bidirectional_lstm(args)?;
-    assert_tensor(&output, &answer, 3, 0.0000001)
+    let (output, answer) = run_bidirectional_lstm("bi_lstm_test", &Device::Cpu)?;
+    assert_tensor(&output, &answer, ACCURACY)
 }
 
 fn test_nlayer_bi_lstm() -> Result<()> {
-    let args = Args {
-        input_dim: IN_DIM,
-        hidden_dim: HIDDEN_DIM,
-        layers: 3,
-        batch_size: BATCH_SIZE,
-        seq_len: SEQ_LEN,
-        pt: "bi_lstm_nlayer_test.pt",
-    };
-
-    let (output, answer) = run_bidirectional_lstm(args)?;
-    assert_tensor(&output, &answer, 3, 0.0000001)
+    let (output, answer) = run_bidirectional_lstm("bi_lstm_nlayer_test", &Device::Cpu)?;
+    assert_tensor(&output, &answer, ACCURACY)
 }
 
-fn run_gru(args: Args) -> Result<(Tensor, Tensor)> {
-    let vb = args.load_pt()?;
-    let answer = vb.get((args.batch_size, args.seq_len, args.hidden_dim), "output")?;
+fn run_gru(model: &str, device: &Device) -> Result<(Tensor, Tensor)> {
+    let (config, vb) = load_model(model, device)?;
+    let answer = vb.get(
+        (config.batch_size, config.sequence_length, config.hidden),
+        "output",
+    )?;
 
-    let mut layers = Vec::with_capacity(args.layers);
+    let mut layers = Vec::with_capacity(config.layers);
 
-    for layer_idx in 0..args.layers {
+    for layer_idx in 0..config.layers {
         let input_dim = if layer_idx == 0 {
-            args.input_dim
+            config.input
         } else {
-            args.hidden_dim
+            config.hidden
         };
-        let config = gru_config(layer_idx, rnn::Direction::Forward);
-        let gru = candle_nn::gru(input_dim, args.hidden_dim, config, vb.clone())?;
+        let gru_config = gru_config(layer_idx, rnn::Direction::Forward);
+        let gru = candle_nn::gru(input_dim, config.hidden, gru_config, vb.clone())?;
         layers.push(gru);
     }
 
-    let mut input = vb.get((args.batch_size, args.seq_len, args.input_dim), "input")?;
+    let mut input = vb.get(
+        (config.batch_size, config.sequence_length, config.input),
+        "input",
+    )?;
 
     for layer in &layers {
         let states = layer.seq(&input)?;
@@ -201,117 +197,88 @@ fn run_gru(args: Args) -> Result<(Tensor, Tensor)> {
     Ok((input, answer))
 }
 
-fn run_bidirectional_gru(args: Args) -> Result<(Tensor, Tensor)> {
-    let vb = args.load_pt()?;
+fn run_bidirectional_gru(model: &str, device: &Device) -> Result<(Tensor, Tensor)> {
+    let (config, vb) = load_model(model, device)?;
+
     let answer = vb.get(
-        (args.batch_size, args.seq_len, args.hidden_dim * 2),
+        (config.batch_size, config.sequence_length, config.hidden * 2),
         "output",
     )?;
 
-    let mut layers = Vec::with_capacity(args.layers);
-    for layer_idx in 0..args.layers {
+    let mut layers = Vec::with_capacity(config.layers);
+    for layer_idx in 0..config.layers {
         let input_dim = if layer_idx == 0 {
-            args.input_dim
+            config.input
         } else {
-            args.hidden_dim * 2
+            config.hidden * 2
         };
 
         let forward_config = gru_config(layer_idx, rnn::Direction::Forward);
-        let forward = candle_nn::gru(input_dim, args.hidden_dim, forward_config, vb.clone())?;
+        let forward = candle_nn::gru(input_dim, config.hidden, forward_config, vb.clone())?;
 
         let backward_config = gru_config(layer_idx, rnn::Direction::Backward);
-        let backward = candle_nn::gru(input_dim, args.hidden_dim, backward_config, vb.clone())?;
+        let backward = candle_nn::gru(input_dim, config.hidden, backward_config, vb.clone())?;
 
         layers.push((forward, backward));
     }
 
-    let mut input = vb.get((args.batch_size, args.seq_len, args.input_dim), "input")?;
+    let mut input = vb.get(
+        (config.batch_size, config.sequence_length, config.input),
+        "input",
+    )?;
 
     for (forward, backward) in &layers {
         let forward_states = forward.seq(&input)?;
         let backward_states = backward.seq(&input)?;
-        input = forward.combine_states_to_tensor(&forward_states, &backward_states)?;
+        input = forward.bidirectional_states_to_tensor(&forward_states, &backward_states)?;
     }
 
     Ok((input, answer))
 }
 
 fn test_gru() -> Result<()> {
-    let args = Args {
-        input_dim: IN_DIM,
-        hidden_dim: HIDDEN_DIM,
-        layers: 1,
-        batch_size: BATCH_SIZE,
-        seq_len: SEQ_LEN,
-        pt: "gru_test.pt",
-    };
-    let (output, answer) = run_gru(args)?;
-    assert_tensor(&output, &answer, 3, 0.0000001)
+    let (output, answer) = run_gru("gru_test", &Device::Cpu)?;
+    assert_tensor(&output, &answer, ACCURACY)
 }
 
 fn test_bi_gru() -> Result<()> {
-    let args = Args {
-        input_dim: IN_DIM,
-        hidden_dim: HIDDEN_DIM,
-        layers: 1,
-        batch_size: BATCH_SIZE,
-        seq_len: SEQ_LEN,
-        pt: "bi_gru_test.pt",
-    };
-
-    let (output, answer) = run_bidirectional_gru(args)?;
-    assert_tensor(&output, &answer, 3, 0.000001)
+    let (output, answer) = run_bidirectional_gru("bi_gru_test", &Device::Cpu)?;
+    assert_tensor(&output, &answer, 0.000001)
 }
 
 fn test_nlayer_gru() -> Result<()> {
-    let args = Args {
-        input_dim: IN_DIM,
-        hidden_dim: HIDDEN_DIM,
-        layers: 3,
-        batch_size: BATCH_SIZE,
-        seq_len: SEQ_LEN,
-        pt: "gru_nlayer_test.pt",
-    };
-    let (output, answer) = run_gru(args)?;
-    assert_tensor(&output, &answer, 3, 0.0000001)
+    let (output, answer) = run_gru("gru_nlayer_test", &Device::Cpu)?;
+    assert_tensor(&output, &answer, ACCURACY)
 }
 
 fn test_nlayer_bi_gru() -> Result<()> {
-    let args = Args {
-        input_dim: IN_DIM,
-        hidden_dim: HIDDEN_DIM,
-        layers: 3,
-        batch_size: BATCH_SIZE,
-        seq_len: SEQ_LEN,
-        pt: "bi_gru_nlayer_test.pt",
-    };
-    let (output, answer) = run_bidirectional_gru(args)?;
-    assert_tensor(&output, &answer, 3, 0.0000001)
+    let (output, answer) = run_bidirectional_gru("bi_gru_nlayer_test", &Device::Cpu)?;
+    assert_tensor(&output, &answer, ACCURACY)
 }
 
 fn main() -> Result<()> {
-    println!("test lstm");
+    print!("test lstm: ");
     test_lstm()?;
 
-    println!("test bi lstm");
-    test_bi_lstm()?;
-
-    println!("test gru");
+    print!("test gru: ");
     test_gru()?;
 
-    println!("test_bi_gru");
+    print!("test bi lstm: ");
+    test_bi_lstm()?;
+
+    print!("test_bi_gru: ");
     test_bi_gru()?;
 
-    println!("test_nlayer_lstm");
+    print!("test_nlayer_lstm: ");
     test_nlayer_lstm()?;
 
-    println!("test_nlayer_bi_lstm");
-    test_nlayer_bi_lstm()?;
-
-    println!("test_nlayer_gru");
+    print!("test_nlayer_gru:");
     test_nlayer_gru()?;
 
-    println!("test_nlayer_bi_gru");
+    print!("test_nlayer_bi_lstm: ");
+    test_nlayer_bi_lstm()?;
+
+    print!("test_nlayer_bi_gru: ");
     test_nlayer_bi_gru()?;
 
     Ok(())
